72719a6e8b0d90761c8ba5172c5abc86ea0311c5
Kevin Goslar <kevin.goslar@gmail.com>
2024-07-15 14:39:46 -0500

    oeu

--- src/extension.ts
+++ src/extension.ts
@@ -99,7 +99,7 @@ function documentSaved() {
       runSafe(repeatTest)()
       break
     case ActionOnSave.testCurrentFile:
-      testFile()
+      runSafe(testFile)()
       break
     case ActionOnSave.none:
       break
