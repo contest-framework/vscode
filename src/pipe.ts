7f6a3ced314099bb2438e843a7b31f7db3b33cd1
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-07 14:29:22 -0600

    Less intrusive notifications

--- src/pipe.ts
+++ src/pipe.ts
@@ -2,6 +2,7 @@ import { promises as fs } from "fs"
 import * as path from "path"
 import { UserError } from "./user_error"
 import * as workspace from "./workspace"
+import * as notification from "./notification"
 
 const PIPE_FILENAME = ".contest.tmp"
 
@@ -9,23 +10,24 @@ export async function send(text: string) {
   // get pipe file path
   const wsRoot = workspace.root()
   if (!wsRoot) {
-    throw new UserError("No workspace found")
+    notification.display("No workspace found")
   }
   const pipePath = path.join(wsRoot, PIPE_FILENAME)
   // ensure pipe exists
-  let stat
   try {
-    stat = await fs.stat(pipePath)
+    var stat = await fs.stat(pipePath)
   } catch (e: any) {
     if (e.code === "ENOENT") {
-      throw new UserError("Please start the Contest server first")
+      notification.display("Please start the Contest server first")
     } else {
-      throw new UserError(`Cannot read pipe: ${e}`)
+      notification.display(`Cannot read pipe: ${e}`)
     }
+    return
   }
   // ensure is pipe
   if (!stat.isFIFO()) {
-    throw new UserError(`The file ${pipePath} exists but is not a FIFO pipe.`)
+    notification.display(`The file ${pipePath} exists but is not a FIFO pipe.`)
+    return
   }
   // write to pipe
   await fs.appendFile(pipePath, text, {
